77480f6ac5ff184c79eac691cd2a9981b0697cca
Amir <aemadzadeh@nvidia.com>
2017-11-07 14:08:16 -0800

    Added tolerance to assert.

--- cooley-tukey/2dfft.py
+++ cooley-tukey/2dfft.py
@@ -5,7 +5,7 @@ import numpy as np
 size  = 64
 A = np.zeros((size, size))
 A[0, 0] = 1
-A[0, 1] = 1
+A[1, 1] = 1
 #A[0, 2] = 1
 #A[0, 31] = 1
 #A[1, 0] = 1
@@ -23,8 +23,8 @@ Afft_1 = np.fft.fft(Afft_col, axis = 1)
 Afft_row = np.fft.fft(A, axis = 1)
 Afft_2 = np.fft.fft(Afft_row, axis = 0)
 
-np.testing.assert_allclose(Afft_1, Afft_gt)
-np.testing.assert_allclose(Afft_2, Afft_gt)
+np.testing.assert_allclose(Afft_1, Afft_gt, atol = 1e-5)
+np.testing.assert_allclose(Afft_2, Afft_gt, atol = 1e-5)
 
 Afft_re  = Afft_gt.real
 Afft_im = Afft_gt.imag
