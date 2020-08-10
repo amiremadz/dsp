3137f6aa786107580883a5e054852e17e152d79a
Amir Emadzadeh <amirok@google.com>
2020-08-10 10:43:59 -0700

    Test fit function.

---
+++ DDE/Matlab/fittest.m
@@ -0,0 +1,27 @@
+clear all;
+
+x = [0.81;0.91;0.13;0.91;0.63;0.098;0.28;0.55;...
+0.96;0.96;0.16;0.97;0.96];
+y = [0.17;0.12;0.16;0.0035;0.37;0.082;0.34;0.56;...
+0.15;-0.046;0.17;-0.091;-0.071];
+
+ft = fittype( 'piecewiseLine( x, a, b, c, d, k )' )
+f = fit( x, y, ft, 'StartPoint', [1, 0, 1, 0, 0.5] )
+
+plot( f, x, y )
+
+function y = piecewiseLine(x, a, b, c, d, k)
+% PIECEWISELINE   A line made of two pieces
+% that is not continuous.
+
+y = zeros(size(x));
+
+% This example includes a for-loop and if statement
+% purely for example purposes.
+for i = 1:length(x)
+    if x(i) < k,
+        y(i) = a + b * x(i);
+    else
+        y(i) = c + d * x(i);
+    end
+end
